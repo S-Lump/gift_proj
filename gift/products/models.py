686e188851474d7f3ee144fb9da7700371a14fdb
StupidLamer <grisga2001@gmail.com>
2020-11-02 00:07:02 +0300

    add get_absolute_url method

--- gift/products/models.py
+++ gift/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class Products(models.Model):
@@ -22,6 +23,9 @@ class Products(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse_lazy('category', kwargs={'category_id': self.pk})
 
     def __str__(self): 
         return self.title
