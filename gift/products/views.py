1e9b112d0488f1890f14247835a71aea7d4e7722
StupidLamer <grisga2001@gmail.com>
2020-11-02 05:06:15 +0300

    add Home contoller

--- gift/products/views.py
+++ gift/products/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView 
 
 from .models import Products, Category
 
-# Create your views here.
-def index(request):
-    products = Products.objects.all()
-    context = {
-        'products': products,
-        'title': 'List of products',
-    }
-    return render(request, 'products/index.html', context=context)
+
+# class Home(ListView):
+#     model = Products
+#     template_name = 'products/category.html'
+#     context_object_name = 'categories'
+#     # extra_content = {
+#     #     'title': 'Главная',
+#     # }
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Главная'
+
+# def index(request):
+#     products = Products.objects.all()
+#     context = {
+#         'products': products,
+#         'title': 'List of products',
+#     }
+#     return render(request, 'products/index.html', context=context)
+
+
+class Home(ListView):
+    model = Category
+    template_name = 'products/index.html'
+    context_object_name = 'categories'
+
 
 def get_category(request, category_id):
     products = Products.objects.filter(category_id=category_id)
