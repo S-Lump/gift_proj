98275b5acef6b2a29b703c04ce5df610debedf13
StupidLamer <grisga2001@gmail.com>
2021-01-03 23:38:34 +0300

    add cart on localStorage

--- gift/gift/static/scripts/Scripts.js
+++ gift/gift/static/scripts/Scripts.js
@@ -1,2 +1,49 @@
-document.querySelector(‘.link-detail’).addEventListener(‘click’, e => e.preventDefault())
+const link = document.querySelector('.link-detail')
+const alertAdd = document.querySelector('.alert')
+const items = document.querySelectorAll('.cart-add')
+const cartItems = document.querySelectorAll('.item-cart')
+const sumPrice = document.querySelector('.sum-price')
+const del = document.querySelectorAll('.del')
+let price = 0
+let flag = true
 
+if (link) link.addEventListener('click', e => e.preventDefault())
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', function() {
+    localStorage.setItem(this.dataset.id, 'OK')
+    alertAdd.style.transform = 'translateY(0)'
+    alertAdd.style.opacity = '1'
+    setTimeout(function() {
+      alertAdd.style.transform = 'translateY(-10vw)'
+      alertAdd.style.opacity = '0'
+    }, 3000)
+  })
+}
+
+function updateItems() {
+  for (let i = 0; i < cartItems.length; i++) {
+    if (!localStorage.getItem(cartItems[i].dataset.id)) {
+      cartItems[i].style.display = 'none'
+    } else {
+      cartItems[i].style.display = 'flex'
+      if (flag) {
+        price += +cartItems[i].dataset.price
+      }
+    }
+  }
+  flag = false
+}
+
+updateItems()
+
+sumPrice.textContent = price
+
+for (let i = 0; i < del.length; i++) {
+  del[i].addEventListener('click', function() {
+    localStorage.removeItem(this.dataset.id)
+    updateItems()
+    price -= +this.dataset.price
+    sumPrice.textContent = price
+  })
+}
